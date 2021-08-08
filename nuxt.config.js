781251977e264892a9f2c680ef794561504505ae
Chidike <chidikenwandu@gmail.com>
2021-08-09 00:36:45 +0100

    description

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s | CityFood',
-    title: 'Cityfood',
+    title: 'The Food is for the City',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
